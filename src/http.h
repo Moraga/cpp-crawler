e08c25da14ca36d5e8c965cde2220f982e7b7f2c
Alejandro Moraga <moraga86@gmail.com>
2015-12-09 08:59:22 -0200

    socket and http for bsd

--- src/http.h
+++ src/http.h
@@ -1,3 +1,75 @@
+#ifndef _WIN32
+
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include "url.h"
+
+#define SIZE 1000000
+
+struct http {
+	int sock;
+	struct sockaddr_in server;
+	bool conn(std::string, int);
+	bool write(std::string);
+	std::string read();
+	std::string fetch(std::string);
+};
+
+bool http::conn(std::string host, int port) {
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock == -1)
+		return false;
+	if (inet_addr(host.c_str()) == -1) {
+		struct hostent* he;
+		struct in_addr** addr_list;
+		he = gethostbyname(host.c_str());
+		if (!he)
+			return false;
+		addr_list = (struct in_addr**) he->h_addr_list;
+		for (int i = 0; addr_list[i] != 0; i++) {
+			server.sin_addr = *addr_list[i];
+			break;
+		}
+	}
+	else {
+		server.sin_addr.s_addr = inet_addr(host.c_str());
+	}
+	server.sin_family = AF_INET;
+	server.sin_port = htons(port);
+	if (connect(sock, (struct sockaddr*) &server, sizeof(server)) < 0)
+		return false;
+	return true;
+};
+
+bool http::write(std::string data) {
+	if (send(sock, data.c_str(), strlen(data.c_str()), 0) < 0) {
+		return false;
+	}
+	return true;
+};
+
+std::string http::read() {
+	char buffer[SIZE];
+	int brecv, total = 0;
+	memset(buffer, 0, SIZE);
+	while ((brecv = recv(sock, buffer + total, SIZE - 1, 0)) > 0) {
+		total += brecv;
+	}
+	return std::string(buffer, strlen(buffer));
+};
+
+std::string http::fetch(std::string url) {
+	Url u = Url::parse(url);
+	conn(u.domain, 80);
+	write("GET "+ u.path +" HTTP/1.1\r\nHost: "+ u.domain +"\r\n\r\n");
+	return read();
+};
+
+#endif
+
+#ifdef _WIN32
+
 #include <string>
 #include <winsock2.h>
 #include <windows.h>
@@ -56,3 +128,5 @@ int http (std::string url) {
 
     return 0;
 }
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -3,78 +3,11 @@
 #include "node.h"
 #include "tools.h"
 #include "url.h"
-
-
-
-#include <netdb.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
-
-int x() {
-
-	int sockfd;
-
-	std::string addr = "www.uol.com.br";
-
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
-	if (sockfd == -1) {
-		return -1;
-	}
-
-	struct sockaddr_in server;
-
-	if (inet_addr(addr.c_str()) == -1) {
-		struct hostent *he;
-		struct in_addr **addr_list;
-
-		he = gethostbyname(addr.c_str());
-
-		if (he == NULL) {
-			return -1;
-		}
-
-		addr_list = (struct in_addr**) he->h_addr_list;
-
-		for (unsigned i = 0; addr_list[i] != NULL; ++i) {
-			server.sin_addr = *addr_list[i];
-		}
-	}
-	else {
-		server.sin_addr.s_addr = inet_addr(addr.c_str());
-	}
-
-	server.sin_family = AF_INET;
-	server.sin_port = htons(80);
-
-	if (connect(sockfd, (struct sockaddr*)&server, sizeof(server)) < 0) {
-		std::cout << "connect failed. error";
-		return -1;
-	}
-
-	std::string data = "GET / HTTP/1.1\r\nHost: www.uol.com.br\r\n\r\n\r\n";
-
-	send(sockfd, data.c_str(), strlen(data.c_str()), 0);
-
-	std::cout << "data sent.";
-
-	char buffer[5000000];
-	int total = 0;
-	int brecv;
-
-	while ((brecv = recv(sockfd, buffer + total, sizeof(buffer), 0)) > 0) {
-		total += brecv;
-	}
-
-	std::cout << buffer << "\n";
-
-	return 0;
-}
-
-
+#include "http.h"
 
 int main() {
-	x();
+	http h;
+	std::cout << h.fetch("www.uol.com.br");
 
 	std::string content =
 			"<html lang = pt>"
