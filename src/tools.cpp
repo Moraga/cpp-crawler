946ff96fea84e75fd6d2364369501ce19bb551c2
Alejandro Moraga <moraga86@gmail.com>
2015-12-28 19:38:09 -0200

    add trim, ltrim, rtrim and remove_spaces

--- src/tools.cpp
+++ src/tools.cpp
@@ -7,3 +7,22 @@ void replace(std::string& str, const std::string& from, const std::string& to) {
 		pos += to.length();
 	}
 };
+
+std::string trim(const std::string& str) {
+	return remove_spaces(ltrim(rtrim(str)));
+};
+
+std::string ltrim(const std::string& str) {
+	static const std::regex re("^\\s+");
+	return std::regex_replace(str, re, "");
+}
+
+std::string rtrim(const std::string& str) {
+	static const std::regex re("\\s+$");
+	return std::regex_replace(str, re, "");
+};
+
+std::string remove_spaces(const std::string& str) {
+	static const std::regex re("\\s{2,}");
+	return std::regex_replace(str, re, " ");
+};
--- src/tools.h
+++ src/tools.h
@@ -1,5 +1,14 @@
 #pragma once
 
 #include <string>
+#include <regex>
 
 void replace(std::string&, const std::string&, const std::string&);
+
+std::string trim(const std::string&);
+
+std::string ltrim(const std::string&);
+
+std::string rtrim(const std::string&);
+
+std::string remove_spaces(const std::string&);
